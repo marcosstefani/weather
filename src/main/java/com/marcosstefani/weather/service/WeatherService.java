08b1230190fef00954087942dcc59236f08df603
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2018-11-03 21:28:12 -0300

    Service implementation

---
+++ src/main/java/com/marcosstefani/weather/service/WeatherService.java
@@ -0,0 +1,74 @@
+package com.marcosstefani.weather.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import com.marcosstefani.weather.dto.WeatherAverageDTO;
+import com.marcosstefani.weather.dto.WeatherMapDTO;
+import com.marcosstefani.weather.dto.WeatherMapTimeDTO;
+
+import springfox.documentation.spring.web.json.Json;
+
+@Service
+public class WeatherService {
+
+	private final String URI = "http://api.openweathermap.org/data/2.5/forecast";
+	private final String API_ID = "0b4ba9ce32d3e8c1c8cda4898c290586";
+
+	private final RestTemplate restTemplate;
+
+	public WeatherService(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
+
+	public ResponseEntity<?> weatherForecastAverage(String city) {
+		List<WeatherAverageDTO> result = new ArrayList<WeatherAverageDTO>();
+		try {
+			WeatherMapDTO weatherMap = this.restTemplate.getForObject(this.url(city), WeatherMapDTO.class);
+
+			for (LocalDate reference = LocalDate.now(); 
+					reference.isBefore(LocalDate.now().plusDays(4));
+					reference = reference.plusDays(1)) {
+				final LocalDate ref = reference;
+				List<WeatherMapTimeDTO> collect = weatherMap.getList().stream()
+						.filter(x -> x.getDt().toLocalDate().equals(ref)).collect(Collectors.toList());
+				if (!CollectionUtils.isEmpty(collect)) {
+					result.add(this.average(collect));
+				}
+
+			}
+		} catch (HttpClientErrorException e) {
+			return new ResponseEntity<>(new Json(e.getResponseBodyAsString()), e.getStatusCode());
+		}
+
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	private WeatherAverageDTO average(List<WeatherMapTimeDTO> list) {
+		WeatherAverageDTO result = new WeatherAverageDTO();
+
+		for (WeatherMapTimeDTO item : list) {
+			result.setDate(item.getDt().toLocalDate());
+			result.plusMap(item);
+		}
+
+		result.totalize();
+
+		return result;
+	}
+
+	private String url(String city) {
+		return String.format(URI.concat("?q=%s").concat("&appid=%s").concat("&units=metric"), city, API_ID);
+	}
+
+}
