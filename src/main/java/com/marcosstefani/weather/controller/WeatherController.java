025308e35a2d92fd140a1ee9594128f36fd78cd5
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2018-11-03 21:50:50 -0300

    Swagger informations

--- src/main/java/com/marcosstefani/weather/controller/WeatherController.java
+++ src/main/java/com/marcosstefani/weather/controller/WeatherController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.marcosstefani.weather.service.WeatherService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("weather")
 public class WeatherController {
@@ -17,8 +20,9 @@ public class WeatherController {
 	@Autowired
 	private WeatherService weatherService;
 
+	@ApiOperation("Return a JSON object that gives the weather averages.")
 	@GetMapping(value = "/nearby-forecast", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> weatherForecastAverage(@RequestParam(required = true) String city) {
+	public ResponseEntity<?> weatherForecastAverage(@ApiParam("City's name")  @RequestParam(required = true) String city) {
 		return weatherService.weatherForecastAverage(city);
 	}
 
